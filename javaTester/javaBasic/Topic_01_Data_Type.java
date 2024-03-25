783bec9037a98e3d9fe7decf1a82bbf652c0de78
tuantrankhac <trankhactuan1999x@gmail.com>
2024-03-25 10:57:33 +0700

    init nopcommer

--- javaTester/javaBasic/Topic_01_Data_Type.java
+++ javaTester/javaBasic/Topic_01_Data_Type.java
@@ -1,5 +1,14 @@
 package javaBasic;
 
 public class Topic_01_Data_Type {
+	static int studentNumber;
+	static boolean status;
 
+	public static void main(String[] args) {
+		System.err.println(studentNumber);
+		System.err.println(status);
+	}
+	
 }
+
+
--- testcases/com/nopcommerce/user/User_01_Register_Login.java
+++ testcases/com/nopcommerce/user/User_01_Register_Login.java
@@ -5,13 +5,28 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 
 public class User_01_Register_Login {
+	//Toàn cục
+	String homePageUrl;
+	
+	
 	@BeforeClass
 	public void beforeClass() {
 	}
 	
 	@Test
-	public void TC_01() {
-	}
+
+	
+	
+	  public void TC_01() { //Cục bộ: Sử dụng trong phạm vi của testcase/ hàm
+	  String homePageUrl = "";
+	  
+	  System.out.println(homePageUrl);
+	  
+	  //Block code if(3 < 5) { //Biến cục bộ nhưng chỉ dùng trong khối lệnh này
+	  String homePageTitle = ""; System.out.println(homePageTitle);
+	  
+	  }
+	 
 	@Test
 	public void TC_02() {
 	}
